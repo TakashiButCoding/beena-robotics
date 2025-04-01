69944d32a24b1cb89b76a58c6de4a27d4801fec9
TakashiButCoding <104755341+TakashiButCoding@users.noreply.github.com>
2025-04-01 20:25:54 +0700

    test

--- pages/aboutus.vue
+++ pages/aboutus.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <Title name="ABOUT US" :imgLink="`${useRuntimeConfig().app.baseURL}aboutus.jpg`" />
+        <Title name="ABOUT US" imgLink="/beena-robotics/aboutus.jpg"/>
         <div>
             
         </div>
