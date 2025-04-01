c690bfe8d253a7a150549417df5050add02ae3a6
TakashiButCoding <104755341+TakashiButCoding@users.noreply.github.com>
2025-04-01 18:58:11 +0700

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   app: {
-    // baseURL: '/beena-robotics/',
+    baseURL: '/beena-robotics/',
     head: {
       title: 'Beena Robotics', 
       charset: 'utf-8',
