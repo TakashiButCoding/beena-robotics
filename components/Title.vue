d53c30b761e243b82474a92dac131b31fa7984b3
TakashiButCoding <104755341+TakashiButCoding@users.noreply.github.com>
2025-04-02 13:12:05 +0700

    update banner

--- components/Title.vue
+++ components/Title.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <div class="relative h-[150px] md:h-[300px] overflow-hidden flex items-center justify-center">
-            <img :src="`/beena-robotics/banner/${imgLink}`" :alt="`${name} Banner`" class="brightness-50 w-full h-auto" />
+            <img :src="`/banner/${imgLink}`" :alt="`${name} Banner`" class="brightness-50 w-full h-auto" />
             <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-4xl font-bold">
                 {{ name }}
             </div>
